071bd2cc71b14d47fb5efcdbdc2e6598f64d4527
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-09-05 01:59:35 -0300

    90%

--- src/components/EmailCopied.tsx
+++ src/components/EmailCopied.tsx
@@ -29,7 +29,11 @@ export default function EmailCopied() {
       </button>
       <div className="flex items-center gap-2 text-[#fff7df]">
         <MdEmail size="1.5rem" />
-        <span ref={textRef} className="mobile:text-[12px]">lucascassimiro545@hotmail.com</span>
+        <span ref={textRef}>
+          <a className="no-underline mobile:text-[12px]">
+            lucascassimiro545@hotmail.com
+          </a>
+        </span>
       </div>
     </>
   );
