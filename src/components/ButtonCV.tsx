7e00f4696c5d65494f2ffabf8fa5898696cebf6a
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-09-06 21:06:22 -0300

    Project finished

--- src/components/ButtonCV.tsx
+++ src/components/ButtonCV.tsx
@@ -6,19 +6,13 @@ const pdf = "http://localhost:3000/curriculo.pdf";
 
 export default function ButtonCV() {
   const downloadFile = (url: string) => {
-    fetch(url)
-      .then((response) => response.blob())
-      .then((blob) => {
-        const blobURL = window.URL.createObjectURL(new Blob([blob]));
-
-        const fileName = url.split("/").pop();
-        const aTag = document.createElement("a");
-        aTag.href = blobURL;
-        aTag.setAttribute("download", fileName!);
-        document.body.appendChild(aTag);
-        aTag.click();
-        aTag.remove();
-      });
+    const fileName = url.split("/").pop();
+    const aTag = document.createElement("a");
+    aTag.href = url;
+    aTag.setAttribute("download", fileName!);
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
   };
 
   return (
